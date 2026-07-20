348ae2a03dc2dfa992460513cbf53ba3bd4c6006
Raman 🌵 <ram.wxyz0@gmail.com>
2026-07-20 21:08:34 +0530

    Time: 0 ms (100.00%) | Memory: 58.4 MB (36.37%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function (n) {
+
+    let answer = new Array(n)
+
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            answer[i - 1] = "FizzBuzz"
+        } else if (i % 5 == 0) {
+            answer[i - 1] = "Buzz"
+        } else if (i % 3 == 0) {
+            answer[i - 1] = "Fizz"
+        } else {
+            answer[i - 1] = String(i)
+        }
+    }
+
+    return answer
+
+};
