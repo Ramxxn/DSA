cea8f2599dfe6db3507de2835d210fb4315aa2b7
Raman 🌵 <ram.wxyz0@gmail.com>
2026-07-21 21:22:36 +0530

    Time: 3 ms (98.08%) | Memory: 63.8 MB (57.77%) - LeetSync

--- 9-palindrome-number/palindrome-number.js
+++ 9-palindrome-number/palindrome-number.js
@@ -3,12 +3,23 @@
  * @return {boolean}
  */
 var isPalindrome = function (x) {
-    if(!Number.isInteger(x)){
-        throw new Error("Input must be an integer")
+    // if(!Number.isInteger(x)){
+    //     throw new Error("Input must be an integer")
+    // }
+
+    if (x < 0) return false;
+
+    if (x < 10) return true;
+
+    let reverseX = 0
+    let value = x
+
+    while (value > 0) {
+        let reminder = value % 10;
+        value = Math.floor(value / 10)
+        reverseX = reverseX * 10 + reminder
     }
 
-    let str1 = String(x)
-    let str2 = String(x).split("").reverse().join("");
+    return reverseX == x
 
-    return str1 === str2
 };
