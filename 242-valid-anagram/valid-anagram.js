fca28b97fb08ab5f6eab88246b2916f6fadec610
Raman 🌵 <ram.wxyz0@gmail.com>
2026-07-28 20:17:44 +0530

    Time: 29 ms (17.80%) | Memory: 57.6 MB (50.95%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -7,17 +7,14 @@
 var isAnagram = function (s, t) {
     //valid input validation
     if (s.length !== t.length) return false
-    
-    let str1 = s.split("")
-    let str2 = t.split("")
+
+    let str1 = s.split("").sort()
+    let str2 = t.split("").sort()
 
     for (let i = 0; i < str1.length; i++) {
-        let idx = str2.indexOf(str1[i])
-
-        if (idx === -1) {
+        if(str1[i] !== str2[i]){
             return false
         }
-        str2.splice(idx, 1)
     }
     return true
 };
