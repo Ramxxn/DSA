af46cdc51c0aa2a9c5b3b326ab92fd861861b95b
Raman 🌵 <ram.wxyz0@gmail.com>
2026-07-31 20:56:57 +0530

    Time: 0 ms (100.00%) | Memory: 56.1 MB (38.90%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.js
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.js
@@ -5,15 +5,26 @@
  */
 var intersect = function (nums1, nums2) {
     // Input validation
+
+    let small = nums1;
+    let large = nums2;
+
+    if (nums1.length > nums2.length) {
+        small = nums2;
+        large = nums1;
+    }
+
+    let map = new Map()
     let ans = []
 
-    for (let i = 0; i < nums1.length; i++) {
-        for(let j=0; j< nums2.length; j++){
-            if(nums1[i] === nums2[j]){
-                ans.push(nums2[j])
-                nums2[j] = null
-                break;
-            }
+    for (let num of small) {
+        map.set(num, (map.get(num) || 0) + 1)
+    }
+
+    for (let num of large) {
+        if (map.get(num) > 0) {
+            ans.push(num)
+            map.set(num, (map.get(num) || 0) - 1)
         }
     }
 
