fd8acb52149eb5c21ec6534536c61c9fc3e45950
Raman 🌵 <ram.wxyz0@gmail.com>
2026-07-31 20:34:53 +0530

    Time: 11 ms (7.05%) | Memory: 53.9 MB (93.65%) - LeetSync

---
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function (nums1, nums2) {
+    // Input validation
+    let ans = []
+
+    for (let i = 0; i < nums1.length; i++) {
+        for(let j=0; j< nums2.length; j++){
+            if(nums1[i] === nums2[j]){
+                ans.push(nums2[j])
+                nums2[j] = null
+                break;
+            }
+        }
+    }
+
+    return ans
+};
