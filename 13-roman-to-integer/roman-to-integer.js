95d501a8b61579f98a5d5ea435e48a9f277d11e6
Raman 🌵 <ram.wxyz0@gmail.com>
2026-08-24 21:38:20 +0530

    Time: 4 ms (73.68%) | Memory: 60.5 MB (83.13%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.js
+++ 13-roman-to-integer/roman-to-integer.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 
-let romanValue = {
+const value = {
     I: 1,
     V: 5,
     X: 10,
@@ -13,40 +13,20 @@ let romanValue = {
     M: 1000,
 }
 
-let romanException = {
-    IV: 4,
-    IX: 9,
-    XL: 40,
-    XC: 90,
-    CD: 400,
-    CM: 900,
-}
+// by subtractive notation or subtractive principle in Roman numerals.
 
 var romanToInt = function (s) {
-    let splitValue = s.split("")
     let sum = 0
-    let skipNext = false;
+    for (let i = 0; i < s.length; i++) {
+        let current = value[s[i]]
+        let next = value[s[i + 1]]
 
-    for (let i = 0; i < splitValue.length; i++) {
-
-        if (skipNext) {
-            skipNext = false;
-            continue;
-        }
-
-        let currChar = splitValue[i]
-        if (i != splitValue.length - 1) {
-            const nextChar = splitValue[i + 1]
-            const combineChar = `${currChar}${nextChar}`
-            if (romanException[combineChar]) {
-                sum += romanException[combineChar]
-                skipNext = true;
-            }
-        }
-
-        if (!skipNext) {
-            sum += romanValue[currChar]
+        if (current < next) {
+            sum -= current
+        } else {
+            sum += current
         }
     }
+
     return sum
 };
